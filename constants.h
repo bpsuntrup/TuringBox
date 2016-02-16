108f29d1e29f7db7968812c960e6100bfae39ca2
Benjamin Paul Suntrup <bpsuntrup@outlook.com>
2016-02-15 19:25:00 -0600

    Add a constants.h file
    ...for the organization of global constants and stuff

---
+++ constants.h
@@ -0,0 +1,9 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// A temporary home for magic numbers
+
+// The number of spaces on the tape that are displayed in the window
+#define TM_TAPE_WIDTH 15
+
+#endif
