ebe3352cce4841af14454619a2fe9498cbfd57c7
Benjamin Paul Suntrup <bpsuntrup@outlook.com>
2016-02-16 20:16:29 -0600

    Fill out Table class with necessary methods

--- Table.h
+++ Table.h
@@ -1,8 +1,38 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+#include <vector>
+
+struct machineState_t
+{
+  int state;
+  int symbol;
+};
+
+struct machineAction_t
+{
+  int newState;
+  int newSymbol;
+  char instruction;
+};
+
+struct machineInstruction_t
+{
+  int id;
+  machineState_t state;
+  machineAction_t action;
+};
+
 class Table
 {
+  vector<machineInstruction_t> table;
+
+public:
+  // insert 5-tuple Turing machine instruction, returning an instruction ID
+  int insertInstruction(int state, int symbol, int newState, int newSymbol, 
+                        char instruction);
+  void removeInstruction(int id);
+  machineAction_t getAction(int state, int symbol);
 };
 
 #endif
