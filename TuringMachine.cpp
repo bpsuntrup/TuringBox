71d1c2ddf29c0482d9041bc450e17297e0783e63
Benjamin Paul Suntrup <bpsuntrup@outlook.com>
2016-02-17 20:00:07 -0600

    Stub out methods for TuringMachine class

--- TuringMachine.cpp
+++ TuringMachine.cpp
@@ -5,9 +5,18 @@
 //  void run(Tape*);
 //  TuringMachine();
 
+TuringMachine::TuringMachine()
+{
+  this.table = new Table();
+}
+
 int TuringMachine::insertInstruction(int state, int symbol, int newState,
                                      int newSymbol, char instruction)
 {
   return this.table.insertInstruction(state, symbol, newState, newSymbol,
                                       instruction);
+}
+
+void TuringMachine::run(Tape*)
+{
 }
